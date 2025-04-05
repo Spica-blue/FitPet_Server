e1ac573b1ab7ba53318d58cd3fc2f4c079b38c2a
Spica-blue <heo1356@gmail.com>
2025-04-06 01:40:26 +0900

    redoc에서 두개씩 보이는거 해결

--- app/api/router.py
+++ app/api/router.py
@@ -4,4 +4,4 @@ from app.api.endpoints import users
 router = APIRouter()
 
 # 사용자 관련 API
-router.include_router(users.router, prefix="/users", tags=["users"])
+router.include_router(users.router, prefix="/users")
